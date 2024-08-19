f8da4334509c5d10829944a0210973f656f0d779
Prabal Singh <41672757+prblsing@users.noreply.github.com>
2024-08-19 20:00:53 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-from scheduler import schedule_tweets_for_now, run_scheduler, tweet_ai_news
+from scheduler import schedule_tweets_for_now, tweet_ai_news
 # from retweet_bot import search_and_retweet
 import logging
 
 if __name__ == "__main__":
     try:
         logging.info('Starting tweet scheduling')
-        MAX_TWEETS_PER_DAY = 4
+        MAX_TWEETS_PER_DAY = 2
         schedule_tweets_for_now(tweet_ai_news, MAX_TWEETS_PER_DAY)
 
         # TBD
         # search_and_retweet("AI", max_tweets=2)
 
-        run_scheduler()
+        # run_scheduler()
     except Exception as e:
         logging.critical('Unhandled exception in main application', exc_info=True)
